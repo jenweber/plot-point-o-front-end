24fca1e659be68616a6f75f132e94e7d731ca83b
JenWeber <weberj10@gmail.com>
2016-02-25 09:33:46 -0500

    add provided js template

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-// load sass manifest
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+require('./example');
+
 require('../styles/index.scss');
 
 $(document).ready(() => {
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
 // load manifests
+// scripts
 require('./assets/scripts/index.js');
-require('./assets/styles/index.scss');
 
+// styles
+require('./assets/styles/index.scss');
 
 // attach jQuery globally
 require('expose?$!jquery');
