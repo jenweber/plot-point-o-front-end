80a7cffc4bd70b4363fceb7377c0092c053642ce
JenWeber <weberj10@gmail.com>
2016-02-27 18:41:10 -0500

    display blog posts with handlebars

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-require('./ajax')
+require('./ajax');
 
 require('./example');
 
 require('../styles/index.scss');
 
-let indexJS = function() {
+
 
 $(document).ready(() => {
   console.log('JavaScript is running');
@@ -18,12 +18,12 @@ $('#home a').click(function (e) {
   $(this).tab('show');
 });
 
-$('#spoiler-free a').click(function (e) {
+$('#no-spoilers-posts a').click(function (e) {
   e.preventDefault();
   $(this).tab('show');
 });
 
-$('#plot-analysis a').click(function (e) {
+$('#spoilery-posts a').click(function (e) {
   e.preventDefault();
   $(this).tab('show');
 });
@@ -33,6 +33,54 @@ $('#login').click(function (e) {
   $(this).tab('show');
 });
 
+// handlebars
+let displaySpoileryPosts = function(response){
+  let posts = response.spoilery_posts;
+  let spoileryPostTemplate = require('./spoilery-post.handlebars');
+  $('#spoilery-posts').append(spoileryPostTemplate({
+    posts
+  }));
+
 };
 
-module.exports = indexJS;
+let displayNoSpoilersPosts = function(response){
+  let posts = response.no_spoilers_posts;
+  let noSpoilersPostTemplate = require('./no-spoilers-post.handlebars');
+  $('#no-spoilers-posts').append(noSpoilersPostTemplate({
+    posts
+  }));
+
+};
+
+let getSpoileryPosts = function(){
+  $.ajax({
+    url: "http://localhost:3000/spoilery_posts",
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(posts){
+    displaySpoileryPosts(posts);
+    console.log(posts);
+  });
+};
+
+let getNoSpoilersPosts = function(){
+  $.ajax({
+    url: "http://localhost:3000/no_spoilers_posts",
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(posts){
+    displayNoSpoilersPosts(posts);
+    console.log(posts);
+  });
+};
+
+
+
+$(document).ready(function(){
+  getSpoileryPosts();
+  getNoSpoilersPosts();
+});
+
+
+
+// module.exports = indexJS;
